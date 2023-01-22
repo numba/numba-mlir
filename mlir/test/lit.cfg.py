4aaacbb7682b578794db193c43876be41b5b3c87
Ivan Butygin <ivan.butygin@gmail.com>
2023-01-22 20:10:27 +0100

    rename opt

--- mlir/test/lit.cfg.py
+++ mlir/test/lit.cfg.py
@@ -51,15 +51,17 @@ config.excludes = ["Inputs", "Examples", "CMakeLists.txt", "README.txt", "LICENS
 
 # test_exec_root: The root path where tests should be run.
 config.test_exec_root = os.path.join(config.imex_obj_root, "mlir", "test")
-config.imex_tools_dir = os.path.join(
+config.numba_tools_dir = os.path.join(
     config.imex_obj_root, "mlir", "tools", "level_zero_runner"
 )
-config.imex_opt_dir = os.path.join(config.imex_obj_root, "mlir", "tools", "imex-opt")
+config.numba_opt_dir = os.path.join(
+    config.imex_obj_root, "mlir", "tools", "numba-mlir-opt"
+)
 
 # Tweak the PATH to include the tools dir.
 llvm_config.with_environment("PATH", config.llvm_tools_dir, append_path=True)
 
-tool_dirs = [config.imex_tools_dir, config.llvm_tools_dir, config.imex_opt_dir]
+tool_dirs = [config.numba_tools_dir, config.llvm_tools_dir, config.numba_opt_dir]
 tools = ["level_zero_runner", "imex-opt"]
 
 llvm_config.add_tool_substitutions(tools, tool_dirs)
