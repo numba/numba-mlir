6df831bcd880e9077dc562cc68e638a724bc53af
Ivan Butygin <ivan.butygin@gmail.com>
2023-02-03 16:32:32 +0100

    Improve test scripts (#23)

---
+++ scripts/runlit.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2023 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+
+if __name__ == "__main__":
+    import lit.main
+
+    print("lit version: ", lit.__version__)
+    lit.main.main()
