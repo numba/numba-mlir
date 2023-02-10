214261609e552ce5f5a14233abd32b54fd445e30
Ivan Butygin <ivan.butygin@gmail.com>
2023-02-10 18:31:17 +0100

    Fix package (#38)
    * Relax mkl version requirements
    * Add zlib as explicit dependency
    * Fix `setup.py` invocation

--- numba_mlir/conda-recipe/bld.bat
+++ numba_mlir/conda-recipe/bld.bat
@@ -2,6 +2,6 @@ copy /y "%RECIPE_DIR%"\MKLConfig.cmake "%BUILD_PREFIX%"\Library\lib\cmake\mkl
 if errorlevel 1 exit 1
 
 pushd numba_mlir
-"%PYTHON%" setup.py install
+"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
 popd
