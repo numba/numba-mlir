13ac9b60909c38e6bc6d7ae176dbd65b97d5b071
Ivan Butygin <ivan.butygin@gmail.com>
2023-02-16 17:14:16 +0100

    enable multithreading

--- mlir/lib/Compiler/Compiler.cpp
+++ mlir/lib/Compiler/Compiler.cpp
@@ -24,9 +24,6 @@ struct PassManagerStage {
                    const numba::CompilerContext::Settings &settings,
                    F &&initFunc)
       : pm(&ctx) {
-    // TODO: investigate
-    ctx.disableMultithreading();
-
     pm.enableVerifier(settings.verify);
 
     if (settings.passStatistics)
