4c7711e59f010814442d8d389a4da0eb84d06006
Ivan Butygin <ivan.butygin@gmail.com>
2023-03-23 00:55:36 +0100

    more tests

--- numba_mlir/numba_mlir/mlir/tests/test_gpu.py
+++ numba_mlir/numba_mlir/mlir/tests/test_gpu.py
@@ -323,14 +323,12 @@ def test_math_funcs_unary(op, dtype):
         i = get_global_id(0)
         b[i] = f(a[i])
 
-    _test_unary(
-        func, dtype, "GPUToSpirvPass", lambda ir: ir.count(f"CL.{op}") == 1
-    )
+    _test_unary(func, dtype, "GPUToSpirvPass", lambda ir: ir.count(f"CL.{op}") == 1)
 
 
 @require_gpu
 @pytest.mark.parametrize("op", ["+", "-", "*", "/", "//", "%", "**"])
-@pytest.mark.parametrize("dtype", [np.int32, np.float32])
+@pytest.mark.parametrize("dtype", [np.int32, np.float32, np.float64])
 def test_gpu_ops_binary(op, dtype):
     f = eval(f"lambda a, b: a {op} b")
     inner = kernel_func(f)
