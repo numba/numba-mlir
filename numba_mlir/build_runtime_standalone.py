a86544c6496be75ea7f69ee1fe1ad26274e4a7b2
Ivan Butygin <ivan.butygin@gmail.com>
2023-04-20 23:01:12 +0200

    Refactor sycl runtime build

---
+++ numba_mlir/build_runtime_standalone.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2023 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import sys
+
+import setup_helper
+
+if __name__ == "__main__":
+    install_prefix = sys.argv[1]
+    setup_helper.build_sycl_math_runtime(install_prefix)
