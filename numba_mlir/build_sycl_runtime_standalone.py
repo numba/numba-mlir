e2cd1f48aa05b29ed19644ee4dc428a0f313f2f7
Ivan Butygin <ivan.butygin@gmail.com>
2023-04-23 21:10:10 +0200

    update runtime build

--- numba_mlir/build_sycl_runtime_standalone.py
+++ numba_mlir/build_sycl_runtime_standalone.py
@@ -8,4 +8,5 @@ import setup_helper
 
 if __name__ == "__main__":
     install_prefix = sys.argv[1]
+    setup_helper.build_sycl_runtime(install_prefix)
     setup_helper.build_sycl_math_runtime(install_prefix)
