0168c43993beb76914b9b98a9deba39bcb9d731a
Ivan Butygin <ivan.butygin@gmail.com>
2023-04-24 12:59:50 +0200

    Properly skip opencl reductions

--- numba_mlir/numba_mlir/mlir/tests/test_gpu.py
+++ numba_mlir/numba_mlir/mlir/tests/test_gpu.py
@@ -43,6 +43,16 @@ def require_dpctl(func):
     )(func)
 
 
+_def_device = get_default_device_name()
+
+
+def skip_opencl_reductions(func):
+    global _def_device
+    return pytest.mark.skipif(
+        _def_device.startswith("opencl"), reason="Reduction issue witch OpenCL backend"
+    )(func)
+
+
 _test_values = [
     True,
     False,
@@ -893,15 +903,12 @@ def test_private_memory(blocksize):
 
 
 @require_gpu
+@skip_opencl_reductions
 @pytest.mark.parametrize("group_op", [group.reduce_add])
 @pytest.mark.parametrize("global_size", [1, 2, 4, 27, 67, 101])
 @pytest.mark.parametrize("local_size", [1, 2, 7, 17, 33])
 @pytest.mark.parametrize("dtype", [np.int32, np.int64, np.float32])
 def test_group_func(group_op, global_size, local_size, dtype):
-    if get_default_device_name().startswith("opencl"):
-        # TODO: subgroup reduction are not supported in opencl backend.
-        pytest.skip()
-
     def func(a, b):
         i = get_global_id(0)
         v = group_op(a[i])
@@ -926,8 +933,9 @@ def test_group_func(group_op, global_size, local_size, dtype):
 
 
 def _from_host(arr, buffer):
-    device = get_default_device_name()
-    ret = dpt.usm_ndarray(arr.shape, dtype=arr.dtype, buffer=buffer).to_device(device)
+    ret = dpt.usm_ndarray(arr.shape, dtype=arr.dtype, buffer=buffer).to_device(
+        _def_device
+    )
     ret.usm_data.copy_from_host(arr.reshape((-1)).view("|u1"))
     return ret
 
@@ -1159,6 +1167,7 @@ def test_cfd_reshape():
 
 @pytest.mark.smoke
 @require_dpctl
+@skip_opencl_reductions
 @pytest.mark.parametrize("size", [1, 7, 16, 64, 65, 256, 512, 1024 * 1024])
 def test_cfd_reduce1(size):
     if size == 1:
@@ -1189,6 +1198,7 @@ _shapes = (1, 7, 16, 25, 64, 65)
 
 
 @require_dpctl
+@skip_opencl_reductions
 @parametrize_function_variants(
     "py_func",
     [
