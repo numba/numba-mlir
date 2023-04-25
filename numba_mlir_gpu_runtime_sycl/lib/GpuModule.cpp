6ae75e200ead9c43b73cc7cc224ce1ec1eb687fd
Ivan Butygin <ivan.butygin@gmail.com>
2023-04-25 21:55:26 +0100

    Load L0 dynamically (#96)

--- numba_mlir_gpu_runtime_sycl/lib/GpuModule.cpp
+++ numba_mlir_gpu_runtime_sycl/lib/GpuModule.cpp
@@ -16,7 +16,7 @@
 #include <windows.h>
 #endif // __linux__
 
-#include "LevelZeroWrapper.hpp"
+#include <level_zero/ze_api.h>
 
 #include <CL/cl.h>
 #if __has_include(<sycl/backend/opencl.hpp>)
@@ -84,6 +84,139 @@ private:
   void *_handle = nullptr;
 };
 
+#define DECL_TYPE(func) using func##_T = decltype(&func)
+#define INIT_FUNC(func)                                                        \
+  do {                                                                         \
+    this->func = this->dynLib.getSymbol<func##_T>(#func);                      \
+  } while (false)
+#define DECL_FUNC(func) func##_T func
+
+static constexpr const char *zeLibName =
+#ifdef __linux__
+    "libze_loader.so";
+#elif defined(_WIN32) || defined(_WIN64)
+    "ze_loader.dll";
+#endif
+
+struct ZELoader {
+  DECL_TYPE(zeModuleCreate);
+  DECL_TYPE(zeModuleBuildLogGetString);
+  DECL_TYPE(zeModuleDestroy);
+  DECL_TYPE(zeModuleBuildLogDestroy);
+  DECL_TYPE(zeKernelCreate);
+  DECL_TYPE(zeKernelDestroy);
+  DECL_TYPE(zeKernelSuggestGroupSize);
+
+  ZELoader() : dynLib(zeLibName) {
+    INIT_FUNC(zeModuleCreate);
+    INIT_FUNC(zeModuleBuildLogGetString);
+    INIT_FUNC(zeModuleDestroy);
+    INIT_FUNC(zeModuleBuildLogDestroy);
+    INIT_FUNC(zeKernelCreate);
+    INIT_FUNC(zeKernelDestroy);
+    INIT_FUNC(zeKernelSuggestGroupSize);
+  }
+
+  DECL_FUNC(zeModuleCreate);
+  DECL_FUNC(zeModuleBuildLogGetString);
+  DECL_FUNC(zeModuleDestroy);
+  DECL_FUNC(zeModuleBuildLogDestroy);
+  DECL_FUNC(zeKernelCreate);
+  DECL_FUNC(zeKernelDestroy);
+  DECL_FUNC(zeKernelSuggestGroupSize);
+
+private:
+  DynamicLibHelper dynLib;
+};
+
+static ZELoader &getZeLoader() {
+  static ZELoader loader;
+  return loader;
+}
+
+static std::string getZeErrorString(ze_result_t val) {
+#define ZE_ENUM_VAL(arg)                                                       \
+  case arg:                                                                    \
+    return #arg
+  switch (val) {
+    ZE_ENUM_VAL(ZE_RESULT_SUCCESS);
+    ZE_ENUM_VAL(ZE_RESULT_NOT_READY);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_DEVICE_LOST);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_OUT_OF_HOST_MEMORY);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_OUT_OF_DEVICE_MEMORY);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_MODULE_BUILD_FAILURE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_MODULE_LINK_FAILURE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_DEVICE_REQUIRES_RESET);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_DEVICE_IN_LOW_POWER_STATE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INSUFFICIENT_PERMISSIONS);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_NOT_AVAILABLE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_DEPENDENCY_UNAVAILABLE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_UNINITIALIZED);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_UNSUPPORTED_VERSION);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_UNSUPPORTED_FEATURE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_ARGUMENT);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_NULL_HANDLE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_HANDLE_OBJECT_IN_USE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_NULL_POINTER);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_SIZE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_UNSUPPORTED_SIZE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_UNSUPPORTED_ALIGNMENT);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_SYNCHRONIZATION_OBJECT);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_ENUMERATION);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_UNSUPPORTED_ENUMERATION);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_UNSUPPORTED_IMAGE_FORMAT);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_NATIVE_BINARY);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_GLOBAL_NAME);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_KERNEL_NAME);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_FUNCTION_NAME);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_GROUP_SIZE_DIMENSION);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_GLOBAL_WIDTH_DIMENSION);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_KERNEL_ARGUMENT_INDEX);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_KERNEL_ARGUMENT_SIZE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_KERNEL_ATTRIBUTE_VALUE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_MODULE_UNLINKED);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_INVALID_COMMAND_LIST_TYPE);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_OVERLAPPING_REGIONS);
+    ZE_ENUM_VAL(ZE_RESULT_ERROR_UNKNOWN);
+
+  default:
+    return "Unknown error: " + std::to_string(val);
+  }
+#undef ZE_ENUM_VAL
+}
+
+static void checkZeResult(ze_result_t res, const char *func) {
+  if (res != ZE_RESULT_SUCCESS)
+    reportError(std::string(func) + " failed: " + getZeErrorString(res));
+}
+
+#define CHECK_ZE_RESULT(expr) checkZeResult((expr), #expr)
+
+struct ZeModuleDeleter {
+  void operator()(ze_module_handle_t module) const {
+    CHECK_ZE_RESULT(getZeLoader().zeModuleDestroy(module));
+  }
+};
+using ZeModule =
+    std::unique_ptr<std::remove_pointer_t<ze_module_handle_t>, ZeModuleDeleter>;
+
+struct ZeBuildLogDeleter {
+  void operator()(ze_module_build_log_handle_t log) const {
+    CHECK_ZE_RESULT(getZeLoader().zeModuleBuildLogDestroy(log));
+  }
+};
+using ZeBuildLog =
+    std::unique_ptr<std::remove_pointer_t<ze_module_build_log_handle_t>,
+                    ZeBuildLogDeleter>;
+
+struct ZeKernelDeleter {
+  void operator()(ze_kernel_handle_t module) const {
+    CHECK_ZE_RESULT(getZeLoader().zeKernelDestroy(module));
+  }
+};
+using ZeKernel =
+    std::unique_ptr<std::remove_pointer_t<ze_kernel_handle_t>, ZeKernelDeleter>;
+
 static constexpr const char *clLibName =
 #ifdef __linux__
     "libOpenCL.so";
@@ -92,35 +225,28 @@ static constexpr const char *clLibName =
 #endif
 
 struct OCLLoader {
-  using clCreateProgramWithIL_T = cl_program (*)(cl_context, const void *,
-                                                 size_t, cl_int *);
-  using clReleaseProgram_T = cl_int (*)(cl_program);
-  using clBuildProgram_T = cl_int (*)(cl_program, cl_uint, const cl_device_id *,
-                                      const char *,
-                                      void (*)(cl_program, void *), void *);
-  using clGetProgramBuildInfo_T = cl_int (*)(cl_program, cl_device_id,
-                                             cl_program_build_info, size_t,
-                                             void *, size_t *);
-  using clCreateKernel_T = cl_kernel (*)(cl_program, const char *, cl_int *);
-  using clReleaseKernel_T = cl_int (*)(cl_kernel);
+  DECL_TYPE(clCreateProgramWithIL);
+  DECL_TYPE(clReleaseProgram);
+  DECL_TYPE(clBuildProgram);
+  DECL_TYPE(clGetProgramBuildInfo);
+  DECL_TYPE(clCreateKernel);
+  DECL_TYPE(clReleaseKernel);
 
   OCLLoader() : dynLib(clLibName) {
-    clCreateProgramWithIL =
-        dynLib.getSymbol<clCreateProgramWithIL_T>("clCreateProgramWithIL");
-    clReleaseProgram = dynLib.getSymbol<clReleaseProgram_T>("clReleaseProgram");
-    clBuildProgram = dynLib.getSymbol<clBuildProgram_T>("clBuildProgram");
-    clGetProgramBuildInfo =
-        dynLib.getSymbol<clGetProgramBuildInfo_T>("clGetProgramBuildInfo");
-    clCreateKernel = dynLib.getSymbol<clCreateKernel_T>("clCreateKernel");
-    clReleaseKernel = dynLib.getSymbol<clReleaseKernel_T>("clReleaseKernel");
+    INIT_FUNC(clCreateProgramWithIL);
+    INIT_FUNC(clReleaseProgram);
+    INIT_FUNC(clBuildProgram);
+    INIT_FUNC(clGetProgramBuildInfo);
+    INIT_FUNC(clCreateKernel);
+    INIT_FUNC(clReleaseKernel);
   }
 
-  clCreateProgramWithIL_T clCreateProgramWithIL;
-  clReleaseProgram_T clReleaseProgram;
-  clBuildProgram_T clBuildProgram;
-  clGetProgramBuildInfo_T clGetProgramBuildInfo;
-  clCreateKernel_T clCreateKernel;
-  clReleaseKernel_T clReleaseKernel;
+  DECL_FUNC(clCreateProgramWithIL);
+  DECL_FUNC(clReleaseProgram);
+  DECL_FUNC(clBuildProgram);
+  DECL_FUNC(clGetProgramBuildInfo);
+  DECL_FUNC(clCreateKernel);
+  DECL_FUNC(clReleaseKernel);
 
 private:
   DynamicLibHelper dynLib;
@@ -136,6 +262,7 @@ static std::string getClErrorString(cl_int val) {
   case arg:                                                                    \
     return #arg
   switch (val) {
+    CL_ENUM_VAL(CL_SUCCESS);
     CL_ENUM_VAL(CL_BUILD_PROGRAM_FAILURE);
     CL_ENUM_VAL(CL_INVALID_CONTEXT);
     CL_ENUM_VAL(CL_INVALID_DEVICE);
@@ -162,15 +289,14 @@ struct ClProgramDeleter {
     CHECK_CL_RESULT(getClLoader().clReleaseProgram(program));
   }
 };
+using ClProgram =
+    std::unique_ptr<std::remove_pointer_t<cl_program>, ClProgramDeleter>;
 
 struct ClKernelDeleter {
   void operator()(cl_kernel kernel) const {
     CHECK_CL_RESULT(getClLoader().clReleaseKernel(kernel));
   }
 };
-
-using ClProgram =
-    std::unique_ptr<std::remove_pointer_t<cl_program>, ClProgramDeleter>;
 using ClKernel =
     std::unique_ptr<std::remove_pointer_t<cl_kernel>, ClKernelDeleter>;
 
@@ -181,13 +307,13 @@ static constexpr const auto cl_be = sycl::backend::opencl;
 struct GPUModule {
   sycl::queue *queue = nullptr;
   sycl::kernel_bundle<sycl::bundle_state::executable> kernelBundle;
-  ze::Module l0module;
+  ZeModule zeModule;
   ClProgram clProgram;
 };
 
 struct GPUKernel {
   sycl::kernel syclKernel;
-  ze::Kernel l0kernel;
+  ZeKernel zeKernel;
   ClKernel clKernel;
   uint32_t maxWgSize = 0;
 };
@@ -198,19 +324,44 @@ GPUModule *createGPUModule(sycl::queue &queue, const void *data,
   auto backend = ctx.get_platform().get_backend();
 
   if (backend == ze_be) {
+    auto &loader = getZeLoader();
     auto zeDevice = sycl::get_native<ze_be>(queue.get_device());
     auto zeContext = sycl::get_native<ze_be>(queue.get_context());
 
     ze_module_desc_t desc = {};
+    desc.stype = ZE_STRUCTURE_TYPE_MODULE_DESC;
     desc.format = ZE_MODULE_FORMAT_IL_SPIRV;
     desc.pInputModule = static_cast<const uint8_t *>(data);
     desc.inputSize = dataSize;
 
-    auto l0module = ze::Module::create(zeContext, zeDevice, desc).first;
+    ze_module_handle_t moduleHandle = nullptr;
+    ze_module_build_log_handle_t logHandle = nullptr;
+    try {
+      CHECK_CL_RESULT(loader.zeModuleCreate(zeContext, zeDevice, &desc,
+                                            &moduleHandle, &logHandle));
+    } catch (std::exception &e) {
+      if (!logHandle)
+        throw;
+
+      ZeBuildLog log(logHandle);
+
+      size_t len = 0;
+      CHECK_ZE_RESULT(
+          loader.zeModuleBuildLogGetString(log.get(), &len, nullptr));
+
+      std::string str;
+      str.resize(len);
+      CHECK_ZE_RESULT(
+          loader.zeModuleBuildLogGetString(log.get(), &len, str.data()));
+      throw std::runtime_error(e.what() + std::string("\n") + str);
+    }
+    ZeBuildLog log(logHandle);
+    ZeModule zeModule(moduleHandle);
+
     auto kernelBundle =
         sycl::make_kernel_bundle<ze_be, sycl::bundle_state::executable>(
-            {l0module.get()}, ctx);
-    return new GPUModule{&queue, std::move(kernelBundle), std::move(l0module),
+            {zeModule.get()}, ctx);
+    return new GPUModule{&queue, std::move(kernelBundle), std::move(zeModule),
                          nullptr};
   }
   if (backend == cl_be) {
@@ -265,12 +416,17 @@ GPUKernel *getGPUKernel(GPUModule *mod, const char *name) {
 
   auto maxWgSize = static_cast<uint32_t>(
       queue->get_device().get_info<sycl::info::device::max_work_group_size>());
-  if (mod->l0module) {
-    auto zeModule = mod->l0module.get();
+  if (mod->zeModule) {
+    auto &loader = getZeLoader();
+    auto zeModule = mod->zeModule.get();
 
     ze_kernel_desc_t desc = {};
+    desc.stype = ZE_STRUCTURE_TYPE_KERNEL_DESC;
     desc.pKernelName = name;
-    auto zeKernel = ze::Kernel::create(zeModule, desc);
+
+    ze_kernel_handle_t kernelHandle = nullptr;
+    CHECK_CL_RESULT(loader.zeKernelCreate(zeModule, &desc, &kernelHandle));
+    ZeKernel zeKernel(kernelHandle);
 
     auto syclKernel =
         sycl::make_kernel<ze_be>({mod->kernelBundle, zeKernel.get()}, ctx);
@@ -319,8 +475,8 @@ static uint32_t upPow2(uint32_t x) {
 void suggestGPUBlockSize(GPUKernel *kernel, const uint32_t *gridSize,
                          uint32_t *blockSize, size_t numDims) {
   assert(numDims > 0 && numDims <= 3);
-  if (kernel->l0kernel) {
-    auto l0Kernel = kernel->l0kernel.get();
+  if (kernel->zeKernel) {
+    auto zeKernel = kernel->zeKernel.get();
     uint32_t gSize[3] = {};
     uint32_t *bSize[3] = {};
     for (size_t i = 0; i < numDims; ++i) {
@@ -328,8 +484,8 @@ void suggestGPUBlockSize(GPUKernel *kernel, const uint32_t *gridSize,
       bSize[i] = &blockSize[i];
     }
 
-    CHECK_ZE_RESULT(zeKernelSuggestGroupSize(
-        l0Kernel, gSize[0], gSize[1], gSize[2], bSize[0], bSize[1], bSize[2]));
+    CHECK_ZE_RESULT(getZeLoader().zeKernelSuggestGroupSize(
+        zeKernel, gSize[0], gSize[1], gSize[2], bSize[0], bSize[1], bSize[2]));
     return;
   }
 
