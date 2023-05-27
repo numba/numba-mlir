18f675679466e096c6f456c8d8b8d5a06ce86385
Ivan Butygin <ivan.butygin@gmail.com>
2023-05-27 22:06:33 +0200

    fix

--- numba_mlir/conda-recipe/bld.bat
+++ numba_mlir/conda-recipe/bld.bat
@@ -1,10 +1,7 @@
-echo %CONDA_PREFIX%
-echo %BUILD_PREFIX%
-
-copy /y "%RECIPE_DIR%"\MKLConfig.cmake "%CONDA_PREFIX%"\Library\lib\cmake\mkl
+copy /y "%RECIPE_DIR%"\MKLConfig.cmake "%BUILD_PREFIX%"\Library\lib\cmake\mkl
 if errorlevel 1 exit 1
 
-set "LIB=%CONDA_PREFIX%\Library\lib;%CONDA_PREFIX%\compiler\lib;%LIB%"
+set "LIB=%BUILD_PREFIX%\Library\lib;%BUILD_PREFIX%\compiler\lib;%LIB%"
 set "INCLUDE=%BUILD_PREFIX%\include;%INCLUDE%"
 
 pushd numba_mlir
