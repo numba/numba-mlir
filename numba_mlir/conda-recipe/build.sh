24d17f0365d815e5b81f29ad77c3e65d6607e063
Ivan Butygin <ivan.butygin@gmail.com>
2023-05-31 12:54:15 +0200

    Set ipcx cfg

--- numba_mlir/conda-recipe/build.sh
+++ numba_mlir/conda-recipe/build.sh
@@ -1,3 +1,9 @@
 pushd numba_mlir
+
+# Intel LLVM must cooperate with compiler and sysroot from conda
+echo "--gcc-toolchain=${BUILD_PREFIX} --sysroot=${BUILD_PREFIX}/${HOST}/sysroot -target ${HOST}" > icpx_for_conda.cfg
+export ICPXCFG="$(pwd)/icpx_for_conda.cfg"
+export ICXCFG="$(pwd)/icpx_for_conda.cfg"
+
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 popd
