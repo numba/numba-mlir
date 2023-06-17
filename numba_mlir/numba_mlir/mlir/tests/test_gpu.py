39d16cb85d082f8f3fa53c09ddbd915a64ec4245
Ivan Butygin <ivan.butygin@gmail.com>
2023-06-17 13:49:26 +0200

    Properly propagate `fp64_truncate` flag in nested functions.

--- numba_mlir/numba_mlir/mlir/tests/test_gpu.py
+++ numba_mlir/numba_mlir/mlir/tests/test_gpu.py
@@ -1307,6 +1307,49 @@ def test_cfd_f64_truncate():
 
 
 @require_dpctl
+def test_cfd_f64_truncate_indirect():
+    def py_func_inner(a, b, c):
+        c[:] = a + b
+
+    jit_func_inner = njit_orig(py_func_inner, gpu_fp64_truncate=True)
+
+    def py_func(a, b, c):
+        jit_func_inner(a, b, c)
+
+    jit_func1 = njit_orig(py_func, gpu_fp64_truncate=True)
+    jit_func2 = njit_orig(py_func, gpu_fp64_truncate=True)
+
+    a = np.arange(1024, dtype=np.float32)
+    b = 2.5
+
+    sim_res = np.zeros(a.shape, a.dtype)
+    py_func(a, b, sim_res)
+
+    da = _from_host(a, buffer="device")
+
+    gpu_res = np.zeros(a.shape, a.dtype)
+    dgpu_res = _from_host(gpu_res, buffer="device")
+
+    pattern = "arith.addf %[0-9a-zA-Z]+, %[0-9a-zA-Z]+ : f32"
+
+    with print_pass_ir(["TruncateF64ForGPUPass"], []):
+        jit_func1(da, b, dgpu_res)
+        ir = get_print_buffer()
+        assert re.search(pattern, ir) is None, ir
+
+    _to_host(dgpu_res, gpu_res)
+    assert_equal(gpu_res, sim_res)
+
+    with print_pass_ir([], ["TruncateF64ForGPUPass"]):
+        jit_func2(da, b, dgpu_res)
+        ir = get_print_buffer()
+        assert re.search(pattern, ir), ir
+
+    _to_host(dgpu_res, gpu_res)
+    assert_equal(gpu_res, sim_res)
+
+
+@require_dpctl
 def test_cfd_reshape():
     def py_func1(a):
         b = a.reshape(17, 23, 56)
