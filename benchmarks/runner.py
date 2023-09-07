9ca813f2972e438b87c97faeb531767884e2f4c1
Ivan Butygin <ivan.butygin@gmail.com>
2023-09-07 19:22:40 +0200

    update runner

--- benchmarks/runner.py
+++ benchmarks/runner.py
@@ -8,20 +8,36 @@ import sys
 import subprocess
 
 
-def run_asv(args):
+def asv_run(args):
     subprocess.check_call(["python", "-m", "asv", "run"] + args)
+
+
+def asv_show(args):
+    subprocess.check_call(["python", "-m", "asv", "show"] + args)
+
+
+def get_head_hash():
+    return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
 
 
 def run_test(params):
     os.environ["NUMBA_MLIR_BENCH_PRESETS"] = "S"
     os.environ["NUMBA_MLIR_BENCH_VALIDATE"] = "1"
-    run_asv(["--python=same", "--quick", "--show-stderr", "--dry-run"])
+    asv_run(["--python=same", "--quick", "--show-stderr", "--dry-run"])
 
 
 def run_bench(params):
     os.environ["NUMBA_MLIR_BENCH_PRESETS"] = "S,M,paper"
     os.environ["NUMBA_MLIR_BENCH_VALIDATE"] = "0"
-    run_asv(["--python=same", "--show-stderr", "--dry-run"])
+    commit = get_head_hash()
+    asv_run(
+        [
+            "--environment=existing:python",
+            "--show-stderr",
+            f"--set-commit-hash={commit}",
+        ]
+    )
+    asv_show([commit])
 
 
 def run_cmd(cmd, params):
@@ -38,8 +54,6 @@ def run_cmd(cmd, params):
 
 
 if __name__ == "__main__":
-    import numba_mlir
-
     args = sys.argv[1:]
     cmd = args[0]
     run_cmd(cmd, args[1:])
