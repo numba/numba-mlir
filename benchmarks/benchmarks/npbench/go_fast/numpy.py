6ca8f6798340fc04b3a0bee91babdedc3252eff5
Ivan Butygin <ivan.butygin@gmail.com>
2023-09-08 20:57:57 +0200

    go_fast

---
+++ benchmarks/benchmarks/npbench/go_fast/numpy.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2012-2020 Anaconda, Inc. and others
+# SPDX-FileCopyrightText: 2021 ETH Zurich and the NPBench authors
+# SPDX-FileCopyrightText: 2022 - 2023 Intel Corporation
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+# https://numba.readthedocs.io/en/stable/user/5minguide.html
+
+from .common import initialize, get_impl, parameters, presets
+import numba_mlir.mlir.benchmarking
+from numba_mlir.mlir.benchmarking import get_numpy_context
+
+
+class Benchmark(numba_mlir.mlir.benchmarking.BenchmarkBase):
+    params = presets
+    param_names = ["preset"]
+
+    def get_func(self, preset):
+        return get_impl(get_numpy_context())
+
+    def initialize(self, preset):
+        preset = parameters[preset]
+        return initialize(**preset)
+
+    def validate(self, args, res):
+        # Assume numpy impl is valid
+        pass
