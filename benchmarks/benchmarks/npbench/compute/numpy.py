95539cf5a7bafe763707c4386d287ae8a9a2a818
Ivan Butygin <ivan.butygin@gmail.com>
2023-09-08 15:56:10 +0200

    compute

---
+++ benchmarks/benchmarks/npbench/compute/numpy.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2023 Stefan Behnel, Robert Bradshaw,
+#   Dag Sverre Seljebotn, Greg Ewing, William Stein, Gabriel Gellner, et al.
+# SPDX-FileCopyrightText: 2021 ETH Zurich and the NPBench authors
+# SPDX-FileCopyrightText: 2022 - 2023 Intel Corporation
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+# https://cython.readthedocs.io/en/latest/src/userguide/numpy_tutorial.html
+
+from .common import initialize, get_impl, parameters, presets
+import numba_mlir.mlir.benchmarking
+from numba_mlir.mlir.benchmarking import get_numpy_context
+
+
+class Benchmark(numba_mlir.mlir.benchmarking.BenchmarkBase):
+    params = presets
+    param_names = ["preset"]
+
+    def get_func(self, preset):
+        return get_impl(get_numpy_context())
+
+    def initialize(self, preset):
+        preset = parameters[preset]
+        return initialize(**preset)
+
+    def validate(self, args, res):
+        # Assume numpy impl is valid
+        pass
