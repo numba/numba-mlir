a312ad3e38be4172fd5356d63eb8bffb25d9e657
Ivan Butygin <ivan.butygin@gmail.com>
2023-09-09 01:55:17 +0200

    nbody

---
+++ benchmarks/benchmarks/npbench/nbody/numpy.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2020 Philip Mocz
+# SPDX-FileCopyrightText: 2021 ETH Zurich and the NPBench authors
+# SPDX-FileCopyrightText: 2022 - 2023 Intel Corporation
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from .common import initialize, get_impl, parameters, presets
+import numba_mlir.mlir.benchmarking
+from numba_mlir.mlir.benchmarking import get_numpy_context
+
+
+class Benchmark(numba_mlir.mlir.benchmarking.BenchmarkBase):
+    params = presets
+    param_names = ["preset"]
+
+    def get_func(self, preset):
+        return get_impl(get_numpy_context())
+
+    def initialize(self, preset):
+        preset = parameters[preset]
+        return initialize(**preset)
+
+    def validate(self, args, res):
+        # Assume numpy impl is valid
+        pass
