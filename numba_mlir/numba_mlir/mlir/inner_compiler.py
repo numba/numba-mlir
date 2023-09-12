c84a189c26775d91c837e43a4343436e16162ed2
Ivan Butygin <ivan.butygin@gmail.com>
2023-09-12 16:15:49 +0200

    MlirTempCompiler

--- numba_mlir/numba_mlir/mlir/inner_compiler.py
+++ numba_mlir/numba_mlir/mlir/inner_compiler.py
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-import functools
-
 from numba.core.untyped_passes import ReconstructSSA
 from numba.core.typed_passes import NopythonTypeInference
 from numba.core.compiler import (
@@ -20,29 +18,24 @@ from numba_mlir.mlir.passes import MlirBackendInner, get_mlir_func
 from .target import numba_mlir_target
 
 
-@functools.lru_cache
-def get_temp_backend():
-    class MlirTempCompiler(CompilerBase):  # custom compiler extends from CompilerBase
-        def define_pipelines(self):
-            dpb = DefaultPassBuilder
-            pm = PassManager("MlirTempCompiler")
-            untyped_passes = dpb.define_untyped_pipeline(self.state)
-            pm.passes.extend(untyped_passes.passes)
+class MlirTempCompiler(CompilerBase):  # custom compiler extends from CompilerBase
+    def define_pipelines(self):
+        dpb = DefaultPassBuilder
+        pm = PassManager("MlirTempCompiler")
+        untyped_passes = dpb.define_untyped_pipeline(self.state)
+        pm.passes.extend(untyped_passes.passes)
 
-            pm.add_pass(ReconstructSSA, "ssa")
-            pm.add_pass(NopythonTypeInference, "nopython frontend")
-            pm.add_pass(MlirBackendInner, "mlir backend")
+        pm.add_pass(ReconstructSSA, "ssa")
+        pm.add_pass(NopythonTypeInference, "nopython frontend")
+        pm.add_pass(MlirBackendInner, "mlir backend")
 
-            pm.finalize()
-            return [pm]
-
-    return MlirTempCompiler
+        pm.finalize()
+        return [pm]
 
 
 def _compile_isolated(func, args, return_type=None, flags=DEFAULT_FLAGS, locals={}):
     typingctx = numba_mlir_target.typing_context
     targetctx = numba_mlir_target.target_context
-    pipeline = get_temp_backend()
 
     return compile_extra(
         typingctx,
@@ -52,7 +45,7 @@ def _compile_isolated(func, args, return_type=None, flags=DEFAULT_FLAGS, locals=
         return_type,
         flags,
         locals,
-        pipeline_class=pipeline,
+        pipeline_class=MlirTempCompiler,
     )
 
 
