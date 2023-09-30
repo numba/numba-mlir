61d34a73a2ecf50ec0629372d0223de077d5e4de
Ivan Butygin <ivan.butygin@gmail.com>
2023-09-30 18:49:30 +0200

    Add covariance WL

---
+++ benchmarks/benchmarks/polybench/datamining/covariance/common.py
@@ -0,0 +1,61 @@
+# SPDX-FileCopyrightText: 2010-2016 Ohio State University
+# SPDX-FileCopyrightText: 2021 ETH Zurich and the NPBench authors
+# SPDX-FileCopyrightText: 2022 - 2023 Intel Corporation
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+from numba_mlir.mlir.benchmarking import parse_config, filter_presets
+from os import path
+import numpy as np
+
+config = parse_config(path.join(path.dirname(__file__), "config.toml"))
+parameters = dict(config["benchmark"]["parameters"])
+presets = filter_presets(parameters.keys())
+
+
+def initialize(M, N, datatype=np.float64):
+    float_n = datatype(N)
+    data = np.fromfunction(lambda i, j: (i * j) / M, (N, M), dtype=datatype)
+
+    return M, float_n, data
+
+
+def get_impl(ctx):
+    jit = ctx.jit
+    np = ctx.numpy
+    prange = ctx.prange
+
+    def kernel(M, float_n, data):
+        mean = np.mean(data, axis=0)
+        data -= mean
+        cov = np.zeros((M, M), dtype=data.dtype)
+        for i in prange(M):
+            cov[i:M, i] = cov[i, i:M] = data[:, i] @ data[:, i:M] / (float_n - 1.0)
+
+        return cov
+
+    return jit(kernel)
+
+
+def get_impl_numba(ctx):
+    jit = ctx.jit
+    np = ctx.numpy
+    prange = ctx.prange
+
+    def kernel(M, float_n, data):
+        # mean = np.mean(data, axis=0)
+        mean = np.sum(data, axis=0) / float_n
+        data -= mean
+        cov = np.zeros((M, M), dtype=data.dtype)
+        # for i in nb.prange(M):
+        #     for j in nb.prange(i, M):
+        #         cov[i, j] = np.sum(data[:, i] * data[:, j])
+        #         cov[i, j] /= float_n - 1.0
+        #         cov[j, i] = cov[i, j]
+        for i in prange(M):
+            cov[i:M, i] = cov[i, i:M] = data[:, i] @ data[:, i:M] / (float_n - 1.0)
+
+        return cov
+
+    return jit(kernel)
