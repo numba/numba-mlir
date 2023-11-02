e0bc77c2214f2d7d53c94f6b2c4ae24593974fd5
Ivan Butygin <ivan.butygin@gmail.com>
2023-11-02 15:17:14 +0300

    Improve caching in tests (#318)

--- numba_mlir/numba_mlir/mlir/tests/utils.py
+++ numba_mlir/numba_mlir/mlir/tests/utils.py
@@ -15,11 +15,35 @@ def parametrize_function_variants(name, strings):
     return pytest.mark.parametrize(name, funcs, ids=strings)
 
 
-def code_or_obj(func):
+class _Wrapper:
+    def __init__(self, value):
+        self._value = value
+        self._hash = hash(value)
+
+    def __eq__(self, obj):
+        return self._value is obj
+
+    def __hash__(self):
+        return self._hash
+
+
+def _get_closure_key(closure):
+    if closure:
+        return tuple(map(lambda a: _Wrapper(a.cell_contents), closure))
+
+    return closure
+
+
+def _get_key(func):
     try:
-        return func.__code__
+        return (func.__code__,) + _get_closure_key(func.__closure__)
     except:
         return func
+
+
+def _del_prev_items(cache, max_size):
+    while len(cache) >= max_size:
+        del cache[next(iter(cache))]
 
 
 class JitfuncCache:
@@ -30,12 +54,19 @@ class JitfuncCache:
     def cached_decorator(self, func, *args, **kwargs):
         if args or kwargs:
             return self._decorator(func, *args, **kwargs)
-        cached = self._cached_funcs.get(func)
+
+        key = _get_key(func)
+        cached = self._cached_funcs.get(key)
         if cached is not None:
+            # Move to front
+            del self._cached_funcs[key]
+            self._cached_funcs[key] = cached
             return cached
 
+        _del_prev_items(self._cached_funcs, 64)
         jitted = self._decorator(func)
-        self._cached_funcs[func] = jitted
+
+        self._cached_funcs[key] = jitted
         return jitted
 
 
