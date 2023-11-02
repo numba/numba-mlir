7430785157a19002df2e85f10a9e3db078eb87ef
Ivan Butygin <ivan.butygin@gmail.com>
2023-11-02 04:20:41 +0100

    cleanup old items

--- numba_mlir/numba_mlir/mlir/tests/utils.py
+++ numba_mlir/numba_mlir/mlir/tests/utils.py
@@ -22,6 +22,11 @@ def code_or_obj(func):
         return func
 
 
+def del_prev_items(cache, max_size):
+    while len(cache) >= max_size:
+        del cache[next(iter(cache))]
+
+
 class JitfuncCache:
     def __init__(self, decorator):
         self._cached_funcs = {}
@@ -30,11 +35,17 @@ class JitfuncCache:
     def cached_decorator(self, func, *args, **kwargs):
         if args or kwargs:
             return self._decorator(func, *args, **kwargs)
+
         cached = self._cached_funcs.get(func)
         if cached is not None:
+            # Move to front
+            del self._cached_funcs[func]
+            self._cached_funcs[func] = cached
             return cached
 
         jitted = self._decorator(func)
+        del_prev_items(self._cached_funcs, 64)
+
         self._cached_funcs[func] = jitted
         return jitted
 
